ed28a7a9c62e140d6f81e251976e8e56a2351119
Ulysse Mercadal <ulysse.mercadal@epitech.eu>
2025-12-29 22:30:06 +0100

    feat: vercel deploy for the front

---
+++ portefolio/api/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://portefolio-lac-chi.vercel.app',
+      'https://*.vercel.app',
+    ],
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on port ${port}`);
+}
+bootstrap();
